a54a05b5919923838b82bde7c0ce19354ef1beae
deep9893 <deeppratapsingh0996@gmail.com>
2023-06-25 09:46:35 +0530

    model training & data transformation done

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -5,6 +5,8 @@ from src.utils.utils import read_yaml_file
 from src.exception import AppException
 from src.entity.config_entity import DataIngestionConfig 
 from src.entity.config_entity import DataValidationConfig
+from src.entity.config_entity import DataTransformationConfig
+from src.entity.config_entity import ModelTrainerConfig
 
 #from src.entity.config_entity import , DataTransformationConfig, ModelTrainerConfig
 from src.constant import *
@@ -65,50 +67,50 @@ class AppConfiguration:
             raise AppException(e, sys) from e
 
 
-    # def get_data_transformation_config(self) -> DataTransformationConfig:
-    #     try:
-    #         data_transformation_config = self.configs_info['data_transformation_config']
-    #         data_validation_config = self.configs_info['data_validation_config']
-    #         data_ingestion_config = self.configs_info['data_ingestion_config']
-    #         dataset_dir = data_ingestion_config['dataset_dir']
-    #         artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        try:
+            data_transformation_config = self.configs_info['data_transformation_config']
+            data_validation_config = self.configs_info['data_validation_config']
+            data_ingestion_config = self.configs_info['data_ingestion_config']
+            dataset_dir = data_ingestion_config['dataset_dir']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
           
-    #         clean_data_file_path = os.path.join(artifacts_dir, dataset_dir, data_validation_config['clean_data_dir'],'clean_data.csv')
-    #         transformed_data_dir = os.path.join(artifacts_dir, dataset_dir, data_transformation_config['transformed_data_dir'])
+            clean_data_file_path = os.path.join(artifacts_dir, dataset_dir, data_validation_config['clean_data_dir'],'clean_data.csv')
+            transformed_data_dir = os.path.join(artifacts_dir, dataset_dir, data_transformation_config['transformed_data_dir'])
 
-    #         response = DataTransformationConfig(
-    #             clean_data_file_path = clean_data_file_path,
-    #             transformed_data_dir = transformed_data_dir
-    #         )
+            response = DataTransformationConfig(
+                clean_data_file_path = clean_data_file_path,
+                transformed_data_dir = transformed_data_dir
+            )
 
-    #         logging.info(f"Data Transformation Config: {response}")
-    #         return response
+            logging.info(f"Data Transformation Config: {response}")
+            return response
 
-    #     except Exception as e:
-    #         raise AppException(e, sys) from e
+        except Exception as e:
+            raise AppException(e, sys) from e
 
-    # def get_model_trainer_config(self) -> ModelTrainerConfig:
-    #     try:
-    #         model_trainer_config = self.configs_info['model_trainer_config']
-    #         data_transformation_config = self.configs_info['data_transformation_config']
-    #         data_ingestion_config = self.configs_info['data_ingestion_config']
-    #         dataset_dir = data_ingestion_config['dataset_dir']
-    #         artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        try:
+            model_trainer_config = self.configs_info['model_trainer_config']
+            data_transformation_config = self.configs_info['data_transformation_config']
+            data_ingestion_config = self.configs_info['data_ingestion_config']
+            dataset_dir = data_ingestion_config['dataset_dir']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
 
           
            
-    #         transformed_data_file_dir = os.path.join(artifacts_dir, dataset_dir, data_transformation_config['transformed_data_dir'], 'transformed_data.csv')
-    #         trained_model_dir = os.path.join(artifacts_dir, model_trainer_config['trained_model_dir'])
-    #         trained_model_name = model_trainer_config['trained_model_name']
+            transformed_data_file_dir = os.path.join(artifacts_dir, dataset_dir, data_transformation_config['transformed_data_dir'], 'transformed_data.csv')
+            trained_model_dir = os.path.join(artifacts_dir, model_trainer_config['trained_model_dir'])
+            trained_model_name = model_trainer_config['trained_model_name']
 
-    #         response = ModelTrainerConfig(
-    #             transformed_data_file_dir = transformed_data_file_dir,
-    #             trained_model_dir = trained_model_dir,
-    #             trained_model_name = trained_model_name
-    #         )
+            response = ModelTrainerConfig(
+                transformed_data_file_dir = transformed_data_file_dir,
+                trained_model_dir = trained_model_dir,
+                trained_model_name = trained_model_name
+            )
 
-    #         logging.info(f"Model Trainer Config: {response}")
-    #         return response
+            logging.info(f"Model Trainer Config: {response}")
+            return response
 
-    #     except Exception as e:
-    #         raise AppException(e, sys) from e
+        except Exception as e:
+            raise AppException(e, sys) from e
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -10,10 +10,10 @@ DataValidationConfig = namedtuple("DataValidationConfig", ["clean_data_dir",
                                                          "serialized_objects_dir"])  
 
 
-# DataTransformationConfig = namedtuple("DataTransformationConfig", ["clean_data_file_path",
-#                                                                    "transformed_data_dir"])     
+DataTransformationConfig = namedtuple("DataTransformationConfig", ["clean_data_file_path",
+                                                                   "transformed_data_dir"])     
 
 
-# ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["transformed_data_file_dir",
-#                                                       "trained_model_dir",
-#                                                       "trained_model_name"]) 
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["transformed_data_file_dir",
+                                                      "trained_model_dir",
+                                                      "trained_model_name"]) 
